257c9c949d7ab611ea73432d4846903253832a43
Joel Carneiro <jolasman@hotmail.com>
2016-10-12 12:53:23 +0100

    Por uma razao desconhecida os spots nao dao
    ouvi durante a aula o outro gajo na mesa ao lado que tinha problemas com
    spots tambem. perguntar a alguem

--- laig/reader/XMLscene.js
+++ laig/reader/XMLscene.js
@@ -23,7 +23,7 @@ XMLscene.prototype.init = function (application) {
 };
 
 XMLscene.prototype.updateLights = function () {
-    for(var i = 0; i < [this.omnisLight + count].length; i++) {
+    for(var i = 0; i < 8; i++) {
         this.lights[i].update();
     }
 };
@@ -32,7 +32,7 @@ XMLscene.prototype.initLights = function () {
 
     this.omnisLight = this.graph.arrayOmni;
     this.spots = this.graph.arraySpot;
-    this.count = 0;
+    this.count = 1;
     for(var i= 0; i < this.omnisLight.length; i++){
         this.positionLights = this.omnisLight[i][2];
         this.ambientlight = this.omnisLight[i][3];
@@ -48,30 +48,48 @@ XMLscene.prototype.initLights = function () {
         this.lights[i].setVisible(true);
         this.lights[i].enable();
         this.lights[i].update();
-        this.count = i;
+        this.count++;
     }
 
-    for(var j = 0; j < this.spots.length; j++){
-
-        this.targetspot = this.spots[j][4];
-        this.positionLightsSpot = this.spots[j][5];
-        this.ambientlightSpot = this.spots[j][3];
-        this.diffSpot = this.spots[j][7];
-        this.specSpot = this.spots[j][8];
-
-        this.lights[j+this.count].setSpotDirection(this.targetspot[0],this.targetspot[1],this.targetspot[2]);
-        this.lights[j+ this.count].setPosition(this.positionLights[0],this.positionLights[1],this.positionLights[2],this.positionLights[3]);
-        this.lights[j + this.count].setAmbient(this.ambientlight[0],this.ambientlight[1],this.ambientlight[2],this.ambientlight[3]);
-        this.lights[j + this.count].setDiffuse(this.diff[0],this.diff[1],this.diff[2],this.diff[3]);
-        this.lights[j + this.count].setSpecular(this.spec[0],this.spec[1],this.spec[2],this.spec[3]);
-
-
-        this.lights[i].setVisible(true);
-        this.lights[i].enable();
-        this.lights[i].update();
-
-
-    }
+    // for(var j = 0; j < this.spots.length; j++){
+    //
+    //     this.targetspot = this.spots[j][4];
+    //     this.positionLightsSpot = this.spots[j][5];
+    //     this.ambientlightSpot = this.spots[j][3];
+    //     this.diffSpot = this.spots[j][7];
+    //     this.specSpot = this.spots[j][8];
+    //     this.angleSpot = this.spots[j][2];
+    //     this.eSpot = this.spots[j][3];
+    //
+    //     if(this.omnisLight.length == 0){
+    //         this.lights[j].setSpotDirection(this.targetspot[0], this.targetspot[1], this.targetspot[2]);
+    //         this.lights[j].setPosition(this.positionLightsSpot[0], this.positionLightsSpot[1], this.positionLightsSpot[2], this.positionLightsSpot[3]);
+    //         this.lights[j].setAmbient(this.ambientlightSpot[0], this.ambientlightSpot[1], this.ambientlightSpot[2], this.ambientlightSpot[3]);
+    //         this.lights[j].setDiffuse(this.diffSpot[0], this.diffSpot[1], this.diffSpot[2], this.diffSpot[3]);
+    //         this.lights[j].setSpecular(this.specSpot[0], this.specSpot[1], this.specSpot[2], this.specSpot[3]);
+    //         this.lights[j].setSpotCutOff(this.angleSpot);
+    //         this.lights[j].setSpotExponent(this.eSpot);
+    //
+    //         this.lights[j].setVisible(true);
+    //         this.lights[j].enable();
+    //         this.lights[j].update();
+    //     }else if(this.omnisLight.length > 0) {
+    //
+    //         this.lights[j + this.count].setSpotDirection(this.targetspot[0], this.targetspot[1], this.targetspot[2]);
+    //         this.lights[j + this.count].setPosition(this.positionLightsSpot[0], this.positionLightsSpot[1], this.positionLightsSpot[2], this.positionLightsSpot[3]);
+    //         this.lights[j + this.count].setAmbient(this.ambientlightSpot[0], this.ambientlightSpot[1], this.ambientlightSpot[2], this.ambientlightSpot[3]);
+    //         this.lights[j + this.count].setDiffuse(this.diffSpot[0], this.diffSpot[1], this.diffSpot[2], this.diffSpot[3]);
+    //         this.lights[j + this.count].setSpecular(this.specSpot[0], this.specSpot[1], this.specSpot[2], this.specSpot[3]);
+    //         this.lights[j + this.count].setSpotCutOff(this.angleSpot);
+    //         this.lights[j + this.count].setSpotExponent(this.eSpot);
+    //
+    //
+    //         this.lights[j + this.count].setVisible(true);
+    //         this.lights[j + this.count].enable();
+    //         this.lights[j + this.count].update();
+    //     }
+    //
+    // }
 
     // this.lights[0].setPosition(2, 3, 3, 1);
     // this.lights[0].setDiffuse(1.0,1.0,1.0,1.0);
