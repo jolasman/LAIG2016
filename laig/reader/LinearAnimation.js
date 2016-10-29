20fdeb1aec6ba19ea8c0a8788d9863bdbded777c
dfreddy <fred.p.p.r@gmail.com>
2016-10-29 16:38:12 +0100

    Update function no LinearAnimation.js

--- laig/reader/LinearAnimation.js
+++ laig/reader/LinearAnimation.js
@@ -1,20 +1,22 @@
 function LinearAnimation(scene, span, control_points) {
 
 	Animation.call(this, scene, span);
-	
+
 	this.control_points = control_points; // vetor de movimento
-	
+
 	this.comprimento_segmentos = [];
 	this.direcao_segmentos = [];
 	this.tempo_segmentos = [];
 	this.velocidade_segmentos = [];
-	
+
+	this.prevTime = -1;
 	this.distancia_percorrida = 0;
 	this.distancia_total = 0;
 	this.tempo_decorrido = 0;
-	
+
 	this.velocidade_animacao = this.distancia_total / span;
-	
+
+
 	// calcula-se todas as variaveis de cada segmento
 	for(var i=0; i < control_points.length-1; i++) {
 		this.comprimento_segmentos[i] = this.calculaComprimentoSegmento(control_points[i], control_points[i+1]);
@@ -22,28 +24,28 @@ function LinearAnimation(scene, span, control_points) {
 		this.velocidade_segmentos[i] = this.calculaVelocidadeSegmento(control_points[i], control_points[i+1], this.tempo_segmentos[i]);
 		this.direcao_segmentos[i] = this.calculaDirecaoSegmento(control_points[i], control_points[i+1]);
 	}
-	
+
 	// calcular a distancia total da animaçao, ou seja, o somatorio dos comprimentos de cada segmento
 	for(var i=0; i < comprimento_segmentos.length; i++) {
 		this.distancia_total += this.comprimento_segmentos[i];
 	}
-	
-	
+
+
 	/*****Colocar o objeto na posicao inicial*****/
 	this.current_point = 1;
 	this.current_direction = this.direcao_segmentos[0];
-	this.tempo_do_segmento = 0;
-	
+	this.tempo_no_segmento = 0;
+
 	mat4.identify(this.matrix);
 	mat4.translate(this.matrix, this.matrix, control_points(this.current_point-1));
-	
+
 	// direcionar o objeto apropriadamento segundo a direcao do movimento
 	// roda-se sobre o eixo y para ficar paralelo ao 'chao'
-    mat4.rotate(this.matrix,this.matrix,this.current_direction,[0,1,0]);
-    
-    this.dist_perc_x = control_point[this.current_point-1][0];
-    this.dist_perc_y = control_point[this.current_point-1][1];
-    this.dist_perc_z = control_point[this.current_point-1][2];
+	mat4.rotate(this.matrix,this.matrix,this.current_direction,[0,1,0]);
+
+	this.dist_perc_x = control_point[this.current_point-1][0];
+	this.dist_perc_y = control_point[this.current_point-1][1];
+	this.dist_perc_z = control_point[this.current_point-1][2];
 };
 
 LinearAnimation.prototype = Object.create(Animation.prototype);
@@ -56,7 +58,7 @@ LinearAnimation.prototype.calculaComprimentoSegmento = function(start, end) {
 	vetor[0] = end[0]-start[0];
 	vetor[1] = end[1]-start[1];
 	vetor[2] = end[2]-start[2];
-	
+
 	return Math.sqrt(Math.pow(vetor[0],2) + Math.pow(vetor[1],2), Math.pow(vetor[2],2))
 };
 
@@ -68,7 +70,55 @@ LinearAnimation.prototype.calculaVelocidadeSegmento = function(start, end, seg_t
 	var vel_x = (end[0] - start[0]) / seg_time;
 	var vel_y = (end[1] - start[1]) / seg_time;
 	var vel_z = (end[2] - start[2]) / seg_time;
-	
+
 	return [vel_x, vel_y, vel_z];
 };
+
+/***********************************update das animaçoes*********************************/
+
+LinearAnimation.prototype.update= function(currTime){
+	if(this.prevTime < 0) 
+		this.prevTime = currTime;
+
+	else {
+		var deltaTime = (currTime - this.prevTime) / 1000;
+
+		this.tempoDecorrido += deltaTime;
+
+		this.tempo_do_segmento += deltaTime;
+
+		this.prevTime = currTime;
+
+		if(this.tempo_decorrido < this.span) {
+
+			var incX = this.velocidade_segmentos[this.current_point-1][0] * deltaTime;
+			var incY = this.velocidade_segmentos[this.current_point-1][1] * deltaTime;
+			var incZ = this.velocidade_segmentos[this.current_point-1][2] * deltaTime;
+
+			this.dist_perc_x += incX;
+			this.dist_perc_y += incY;
+			this.dist_perc_z += incZ;
+
+			this.current_direction = this.direcao_segmentos[this.current_point - 1];
+
+			mat4.identity(this.matrix);
+
+			mat4.translate(this.matrix, this.matrix, [this.dist_perc_x, this.dist_perc_y, this.dist_perc_z]);
+
+			mat4.rotate(this.matrix, this.matrix, this.current_direction, [0,1,0]);
+
+			if((this.tempo_no_segmento == this.tempo_segmentos[this.current_point - 1]) && (this.current_point+1 < this.control_points.length)) {
+				this.current_point++;
+				this.tempo_no_segmento = 0;
+			}
+		}
+		else {
+			this.finished=1;
+			return;
+		}
+	}
+};
+
+
+
 
