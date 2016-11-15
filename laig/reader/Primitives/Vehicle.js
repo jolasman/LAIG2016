028bc5fb525a03c3a851c3ff51451ba6e404dd8c
Joel Carneiro <jolasman@hotmail.com>
2016-11-15 00:40:57 +0000

    up

--- laig/reader/Primitives/Vehicle.js
+++ laig/reader/Primitives/Vehicle.js
@@ -7,18 +7,22 @@ function Vehicle(scene)
     this.scene = scene;
 
     var controlPoints1 = [
-        [-1.5,-1.5,0],[-2,-2,2],[-2,2,2],
-        [0,0,3],[3,-2,3],[0,2,3],
-        [1.5,-1.5,0],[2,-2,2], [2,2,2]
+        [-1.5,-1.5,0],[-2,-2,2],[-2,2,2],[-1.5,1.5,0],
+
+        [0,0,3],[0,-2,3],[0,2,3],[0,0,3],
+
+        [1.5,-1.5,0],[2,-2,2], [2,2,2],[1.5,1.5,0]
+
+
     ];
 
     var controlPoints2 =[
-        [-2.5,-2.5,1],[-3,-3,3],[-3,3,3],
-        [1,1,4],[4,-1,4],[1,3,4],
-        [2.5,-2.5,0],[3,-3,3],[3,3,3] ];
+        [-2,-2,1],[-2,-1,-2],[-2,1,5],[-2,2,1],
+        [0,-2, 0],[0,-1,-1],[0,1,1.5],[0,2,0],
+        [2,-2,-1],[2,-1,2],[2,1,-5],[2,2,1] ];
 
-    this.vehicle = new Patch(scene,2,2,20,20,controlPoints1);
-   // this.vehicle = new Patch(scene,2,2,7,9,controlPoints2);
+    this.vehicle = new Patch(scene,2,3,20,20,controlPoints1);
+    // this.vehicle = new Patch(scene,2,3,20,20,controlPoints2);
 
     this.initBuffers();
 };
