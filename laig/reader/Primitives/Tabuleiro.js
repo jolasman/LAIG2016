4a5da46933ab07e579f6acb104c3d59c9de8e170
Joel Carneiro <jolasman@hotmail.com>
2016-11-28 14:47:37 +0000

    Tabuleiro.js

---
+++ laig/reader/Primitives/Tabuleiro.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Joel Carneiro on 12/11/2016.
+ */
+function Tabuleiro(scene)
+
+{
+    CGFobject.call(this,scene);
+    this.scene = scene;
+    this.du= 10.0;
+    this.dv= 10.0;
+    this.su= 2.0;
+    this.sv= 2.0;
+
+    var c1 = [0.0,0.0,0.0,1.0];
+    var c2 = [1.0,1.0,1.0,1.0];
+    var cs = [0.9,0.0,0.0,1.0];
+
+    this.tabuleiroAppearance1 = new CGFappearance(this.scene);
+    this.tabuleiroAppearance1.setTextureWrap('CLAMP_TO_EDGE', 'CLAMP_TO_EDGE');
+    this.tabuleiroAppearance1.loadTexture("./texturas/tecto.png");
+    this.textureref = this.tabuleiroAppearance1;
+
+    //passar valores para o shader
+    this.scene.customShader.setUniformsValues({cor1 :c1, cor2 :c2, cor3 :cs, dims: [this.du,this.dv], dimscs: [this.su,this.sv], texturaref: this.textureref});
+
+    this.chess = new Plane(scene,20,20,90,90); // usar valores altos e multiplos de du e dv no valores de partsX e partsY do Plane, para melhor efeito no levantamento dos vertices
+    this.initBuffers();
+};
+
+Tabuleiro.prototype = Object.create(CGFobject.prototype);
+Tabuleiro.prototype.constructor=Tabuleiro;
+
+Tabuleiro.prototype.initBuffers = function () {};
+
+Tabuleiro.prototype.updateTexCoords=function(length_S,length_T){};
+
+Tabuleiro.prototype.display = function ()
+{
+    //truque manhoso para o shader nao estourar
+    this.scene.setActiveShader(this.scene.customShader);
+    this.scene.pushMatrix();
+    this.tabuleiroAppearance1.apply();
+    this.chess.display();
+    this.scene.popMatrix()
+    this.scene.setActiveShader(this.scene.defaultShader);
+};
