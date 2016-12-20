0518aa0b1ca7731e3d37eade9bcad175362fa968
Joel Carneiro <jolasman@hotmail.com>
2016-12-20 22:45:23 +0000

    up

--- laig/reader/Primitives/Tabuleiro.js
+++ laig/reader/Primitives/Tabuleiro.js
@@ -15,6 +15,9 @@ function Tabuleiro(scene)
         }
     }
     this.arrayTiles[0][0].setTilePeca(new Peca(this.scene));
+    this.arrayTiles[0][1].setTilePeca(new Peca(this.scene));
+    this.arrayTiles[0][2].setTilePeca(new Peca(this.scene));
+    this.arrayTiles[4][0].setTilePeca(new Peca(this.scene));
 
 
     this.esquerdo = new Cubo(scene);
@@ -26,21 +29,16 @@ function Tabuleiro(scene)
     this.recEsq = new MyRectangle(scene,0,1,1,0);
     this.recDir = new MyRectangle(scene,0,1,1,0);
 
-    this.appearance1 = new CGFappearance(this.scene);
-    this.marmore = new CGFtexture(this.scene,"./texturas/marmore.png");
 
-    this.appearance3 = new CGFappearance(this.scene);
-    this.preto = new CGFtexture(this.scene,"./texturas/marmore_preto.png");
 
-    this.agua = new CGFappearance(this.scene);
-    this.agua.setAmbient(0.5,0.5,0.5,1);
-    this.agua.setDiffuse(0.5,0.5,0.5,1);
-    this.agua.setSpecular(0.5,0.5,0.5,1);
-    this.agua.setShininess(8.8);
-    this.agua.setEmission(0,0,0.0,1);
-    this.agua.setTextureWrap('CLAMP_TO_EDGE', 'CLAMP_TO_EDGE');
-    this.agua.loadTexture("./texturas/agua.png");
-
+    this.selectappear = new CGFappearance(this.scene);
+    this.selectappear.setAmbient(0.5,0.5,0.5,1);
+    this.selectappear.setDiffuse(0.5,0.5,0.5,1);
+    this.selectappear.setSpecular(0.5,0.5,0.5,1);
+    this.selectappear.setShininess(8.8);
+    this.selectappear.setEmission(0,0,0.0,1);
+    this.selectappear.setTextureWrap('CLAMP_TO_EDGE', 'CLAMP_TO_EDGE');
+    this.selectappear.loadTexture("./texturas/agua.png");
 
 
 
@@ -70,32 +68,33 @@ Tabuleiro.prototype.display = function ()
 
     for( var i= 0; i<5; i++){
         for(var j = 0; j<5; j++){
-           var k = i+j*5;
+            var k = i*5+j;
             this.scene.pushMatrix();
             this.scene.rotate(-Math.PI/2, 1,0,0);
             this.scene.translate(12-8*i,-20+8*j,2);
             if(this.scene.escolhido == k){
-                this.agua.apply();
+
+
 
             }
 
-             else if((i+j)%2 == 0) {
-                 this.appearance1.setTexture(this.marmore);
-                 this.marmore.bind();
-                   this.arrayTiles[i][j].display();
-                 this.marmore.unbind();
+            else if((i+j)%2 == 0) {
+
+                this.arrayTiles[i][j].display();
+
             }else {
 
-                 this.appearance3.setTexture(this.preto);
-                 this.preto.bind();
-                 this.arrayTiles[i][j].display();
-                 this.preto.unbind();
+
+                this.arrayTiles[i][j].display();
+
             }
 
             this.scene.registerForPick(k, this.arrayTiles[i][j]);
 
             this.scene.popMatrix();
         }
+
+
     }
 
 
@@ -143,7 +142,7 @@ Tabuleiro.prototype.display = function ()
 
     //rec esquerdo
     this.scene.pushMatrix();
-    // this.ladosAppearance.apply();
+    this.ladosAppearance.apply();
     this.scene.rotate(-Math.PI/2, 1,0,0);
     this.scene.rotate(Math.PI/2, 0,0,1);
     this.scene.translate(-25,-20,3.1);
@@ -153,7 +152,7 @@ Tabuleiro.prototype.display = function ()
 
     // rec direito
     this.scene.pushMatrix();
-    // this.ladosAppearance.apply();
+    this.ladosAppearance.apply();
     this.scene.rotate(-Math.PI/2, 1,0,0);
     this.scene.rotate(Math.PI/2, 0,0,1);
     this.scene.translate(20,-20,3.1);
--- laig/reader/Primitives/Tile.js
+++ laig/reader/Primitives/Tile.js
@@ -11,6 +11,24 @@ function Tile(scene, tabuleiro)
 
     this.rec= new MyRectangle(scene,0,8,8,0);
 
+    this.marmoreappearance = new CGFappearance(this.scene);
+    this.marmoreappearance.setAmbient(0.5,0.5,0.5,1);
+    this.marmoreappearance.setDiffuse(0.5,0.5,0.5,1);
+    this.marmoreappearance.setSpecular(0.5,0.5,0.5,1);
+    this.marmoreappearance.setShininess(0.8);
+    this.marmoreappearance.setEmission(0,0,0.0,1);
+    this.marmoreappearance.setTextureWrap('CLAMP_TO_EDGE', 'CLAMP_TO_EDGE');
+    this.marmoreappearance.loadTexture("./texturas/marmore_preto.png");
+
+
+    this.pecaappearance = new CGFappearance(this.scene);
+    this.pecaappearance.setAmbient(0.9,0.1,0.5,1);
+    this.pecaappearance.setDiffuse(0.9,0.1,0.5,1);
+    this.pecaappearance.setSpecular(0.9,0.1,0.5,1);
+    this.pecaappearance.setShininess(0.8);
+    this.pecaappearance.setEmission(0,0,0.0,1);
+
+
     this.initBuffers();
 }
 
@@ -25,9 +43,14 @@ Tile.prototype.display = function ()
 {
     this.scene.pushMatrix();
    if(this.peca != null){
+       this.scene.pushMatrix();
+       this.scene.scale(5,5,5);
+       this.scene.translate(0.5,0.5,0.5);
+       this.pecaappearance.apply();
        this.peca.display();
+       this.scene.popMatrix();
    }
-
+    this.marmoreappearance.apply();
     this.rec.display();
     this.scene.popMatrix();
 
